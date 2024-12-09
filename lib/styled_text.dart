0af0e46e4b8c2d855bba5b269ce42d5bf834d273
jojojojonathan <jonathantheresanto123@gmail.com>
2024-12-09 19:07:40 +0700

    first commit

---
+++ lib/styled_text.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class StyledText extends StatelessWidget {
+  const StyledText(this.text, this.fontSize, this.color, {super.key});
+
+  final String text;
+  final double fontSize;
+  final Color color;
+
+  @override
+  Widget build(context) {
+    return Text(
+      text,
+      style: TextStyle(
+        color: color,
+        fontSize: fontSize,
+      ),
+    );
+  }
+}
